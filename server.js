5f4a1bcf42a0d5e52a35928cbf09eb58416b1d99
Afeez Adewumi <adewumilanre01@gmail.com>
2024-04-23 16:27:43 -0500

    Adding movie finder app

---
+++ server.js
@@ -0,0 +1,59 @@
+const express = require("express")
+const app = express()
+const cors = require("cors")
+const {MongoClient, ObjectId} = require("mongodb")
+require("dotenv").config()
+const PORT = 7860
+
+let db,
+    dbConnectionStr = process.env.DB_STRING,
+    dbName = "sample_mflix",
+    collection 
+
+MongoClient.connect(dbConnectionStr)
+    .then(client => {
+        db = client.db(dbName)
+        console.log(`Connected to database`)
+        collection = db.collection("movies")
+    })
+
+app.use(express.urlencoded({extended : true}))
+app.use(express.json())
+app.use(cors())
+
+app.get("/search", async (req,res) => {
+    try{
+        let result = await collection.aggregate([
+            {"$Search" : {
+                "autocomplete": {
+                    "query" :  `${req.query.query}`,
+                    "path": "title",
+                    "fuzzy": {
+                        "maxEdits":2,
+                        "prefixLength":3
+                    }
+                }
+            }
+        }
+        ]).toArray()
+        res.send(result)
+    }catch (error){
+        res.status(500).send({message: error.message})
+    }
+})
+
+app.get("/get/:id", async (req, res) => {
+    try {
+        let result = await collection.findOne({
+            "i_d": ObjectId(req.params.id)
+        })
+        res.send(result)
+    } catch (error){
+        res.status(500).send({message: error.message})
+    }
+})
+
+
+app.listen(process.env.PORT || PORT, () => {
+            console.log(`Server is running on PORT ${PORT}`)
+        })
